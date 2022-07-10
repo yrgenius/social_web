2417e5b1ab3d858cb4f8803752be852d6c0ca8a9
Oblivion <yrgenius80@gmail.com>
2022-07-10 17:14:42 +0900

    start project branch dev

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
     return(
         <header className='header'>
-            <img src="./logo.svg" alt="logo" className="logo-img" />
+            <img className="logo-img" src="https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png" alt="logo" />
         </header>    
     );
 }
